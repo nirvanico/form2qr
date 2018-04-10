f744712f37b4aa88ab4f818bab1bac75a8d1d350
Unknown <fear.nirva@gmail.com>
2018-04-10 15:56:05 +0200

    some fix

--- js/form2qr.js
+++ js/form2qr.js
@@ -1,19 +1,13 @@
 var qrcode = new QRCode("qrcode");
 
-function makeCode () {      
+function makeCode() {
     var elText = document.getElementById("website");
-    
+
     qrcode.makeCode(elText.value);
 }
 
 makeCode();
 
-$("#website").
-    on("blur", function () {
-        makeCode();
-    }).
-    on("keydown", function (e) {
-        if (e.keyCode == 13) {
-            makeCode();
-        }
-    });
+$("#generateqr").click(function () {
+    makeCode();
+});
